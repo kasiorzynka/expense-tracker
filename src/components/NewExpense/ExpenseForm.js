623decbcd824905618710c5b5dba318ae660580d
kasiorzynka <katarzyna.ficz@gmail.com>
2022-02-10 21:46:24 +0100

    Added update of a state in NewExpense component, handling form submission, added two-way binding, child to parent communication and lifting the state of new expense up. Cleanings.

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,55 +1,37 @@
 import { useState } from "react";
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
-  /* version with just only one state
-  const [userInput, setUserInput] = useState({
-    enteredTitle: "",
-    enteredAmount: "",
-    enteredDate: "",
-  });
-*/
+
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
-    /*
-    setUserInput({
-      ...userInput, //not changed values are kept, otherwise they would be lost in case of just one state for few inputs
-      enteredTitle: event.target.value,
-    });
-    setUserInput((prevState) => {
-      //guarantying that u always get the actual state; use whenever your state update depends on the previous state
-      return { ...userInput, enteredTitle: event.target.value };
-    });
-    */
   };
 
   const amountChangeHandler = (event) => {
     setEnteredAmount(event.target.value);
-    /*
-    setUserInput({
-      ...userInput, //not changed values are kept, otherwise they would be lost in case of just one state for few inputs
-      enteredAmount: event.target.value,
-    });
-     */
   };
 
   const dateChangeHandler = (event) => {
     setEnteredDate(event.target.value);
-    /*
-    setUserInput({
-      ...userInput, //not changed values are kept, otherwise they would be lost in case of just one state for few inputs
-      enteredDate: event.target.value,
-    });
-     */
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
 
-    const expenseData = "";
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    //console.log(expenseData);
+    props.onSaveExpenseData(expenseData);
+
+    setEnteredTitle(""); //Clearing input value after submitting form
+    setEnteredAmount("");
+    setEnteredDate("");
   };
 
   return (
@@ -57,7 +39,11 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -65,6 +51,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -74,6 +61,7 @@ const ExpenseForm = () => {
             type="date"
             min="2021-01-01"
             max="2022-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
