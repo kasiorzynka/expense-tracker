df8d18678569a3e9b5948ef47850eff585d1f5fc
kasiorzynka <katarzyna.ficz@gmail.com>
2022-02-11 00:46:17 +0100

    Added new component ExpensesFilter with two way binding and child-parent communication (with expenses).

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,5 +1,5 @@
-import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
 
 const NewExpense = (props) => {
   const saveExpenseDataHandler = (enteredExpenseData) => {
